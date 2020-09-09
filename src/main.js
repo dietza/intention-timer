16b4d05f7ef3e552ea7908162510a05139d7eeeb
JP Carey <jpcarey4@gmail.com>
2020-09-09 16:24:27 -0600

    Target category buttons in selectActivity function

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
+var newActivityCard = document.querySelector('.new-activity-card');
 
 var currentActivity;
+
+newActivityCard.addEventListener('click', selectActivity);
+
+function selectActivity(event) {
+  if (event.target.closest('.study-button')) {
+    console.log("Hello");
+  } else if (event.target.closest('.meditate-button')) {
+    console.log("hi");
+  } else if (event.target.closest('.exercise-button')) {
+    console.log("hey");
+  }
+}
