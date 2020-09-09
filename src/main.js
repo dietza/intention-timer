da3ab08f54f4b8b16f9229ff6a30ed9066969a57
JP Carey <jpcarey4@gmail.com>
2020-09-09 17:44:02 -0600

    Create handler functions for button colors

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 var newActivityCard = document.querySelector('.new-activity-card');
+var studyButton = document.querySelector('.study-button');
+var meditateButton = document.querySelector('.meditate-button');
+var exerciseButton = document.querySelector('.exercise-button');
 
 var currentActivity;
 
-newActivityCard.addEventListener('click', selectActivity);
+studyButton.addEventListener('click', handleStudyColor);
+meditateButton.addEventListener('click', handleMeditateButton);
+exerciseButton.addEventListener('click', handleExerciseButton);
 
-function selectActivity(event) {
-  if (event.target.closest('.study-button')) {
-  } else if (event.target.closest('.meditate-button')) {
-  } else if (event.target.closest('.exercise-button')) {
-  }
+
+function handleStudyColor() {
+  toggleButtonColor(studyButton, 'study-green');
+}
+
+function handleMeditateButton() {
+  toggleButtonColor(meditateButton, 'meditate-purple');
+}
+
+function handleExerciseButton() {
+  toggleButtonColor(exerciseButton, 'exercise-red');
+}
+
+function toggleButtonColor(button, color) {
+  button.classList.add(color);
 }
