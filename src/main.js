ae4d1f765d64e0b33f4824028c363405898e1c8f
JP Carey <jpcarey4@gmail.com>
2020-09-09 17:06:31 -0600

    clean up commented out code

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ newActivityCard.addEventListener('click', selectActivity);
 
 function selectActivity(event) {
   if (event.target.closest('.study-button')) {
-    event.target.closest('.study-button').classList.add('study-green');
   } else if (event.target.closest('.meditate-button')) {
   } else if (event.target.closest('.exercise-button')) {
   }
