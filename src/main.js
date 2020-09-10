3529a3224cb651f83a27f7d738af4823fc02391f
Alyssa Bull <alyssabull@gmail.com>
2020-09-09 21:25:36 -0600

    Toggle disabled form inputs

--- src/main.js
+++ src/main.js
@@ -1,21 +1,35 @@
 var categoryButton = document.querySelectorAll('.category-button');
+var startActivityButton = document.querySelector('.start-activity-button');
+var intentionInput = document.querySelector('.intention-input');
+var minutesInput = document.querySelector('.minutes-input');
+var secondsInput = document.querySelector('.seconds-input');
 
 var currentActivity;
 
-categoryButton[0].addEventListener('click', handleStudyColor);
+categoryButton[0].addEventListener('click', handleStudyButton);
 categoryButton[1].addEventListener('click', handleMeditateButton);
 categoryButton[2].addEventListener('click', handleExerciseButton);
+startActivityButton.addEventListener('click', handleActivitySubmit);
 
-function handleStudyColor() {
+function handleStudyButton() {
   resetButtonColor(categoryButton[0], 'study-green', "./assets/study-active.svg");
+  enableInputs();
 }
 
 function handleMeditateButton() {
   resetButtonColor(categoryButton[1], 'meditate-purple', "./assets/meditate-active.svg");
+  enableInputs();
 }
 
 function handleExerciseButton() {
   resetButtonColor(categoryButton[2], 'exercise-red', "./assets/exercise-active.svg");
+  enableInputs();
+}
+
+function enableInputs() {
+  intentionInput.disabled = false;
+  minutesInput.disabled = false;
+  secondsInput.disabled = false;
 }
 
 function resetButtonColor(button, color, icon) {
@@ -35,4 +49,14 @@ function addButtonColor(selectedButton, selectedColor, selectedIcon) {
   selectedButton.classList.add(selectedColor);
   selectedButton.children[1].classList.add(selectedColor);
   selectedButton.children[0].src = selectedIcon;
+}
+
+function handleActivitySubmit() {
+  if (intentionInput.value !== '' && (minutesInput.value !== '' && secondsInput.value !== '')){
+
+  }
+}
+
+function removeDisabled() {
+  console.log("hello");
 }
